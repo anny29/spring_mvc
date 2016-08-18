ec6697268c4e43e3a6378ef0cb7ed3e02a72ba80
anny29 <lk753951@hotmail.com>
2016-08-18 23:52:54 +0800

    spring mvc 初始创建

---
+++ src/main/java/com/anny/study/spring/mvc/Test.java
@@ -0,0 +1,12 @@
+package com.anny.study.spring.mvc;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//DispatcherServlet
+	}
+
+}
